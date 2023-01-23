fc757144e22b183ffaf55c52419597387bfbd6fa
Vladyslav <vladyslav.sovpenko@gmail.com>
2023-01-23 18:19:44 +0200

    Initial commit

---
+++ vss/src/test/java/com/vehicle/vss/VssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicle.vss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
